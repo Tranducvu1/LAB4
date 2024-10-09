03a193db49692a5d24af7fc68a1c4154297703b1
Trần Đức Vũ <tranducvuht@gamil.com>
2024-10-09 07:09:08 +0700

    update draw traigles with opacityt

--- lib/main.dart
+++ lib/main.dart
@@ -43,10 +43,73 @@ class _MyHomePageState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextButton(
-        onPressed: updateImage,
-        child: Image.asset("images/ball$ballNumber.png"),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          // This is the outer glow or shadow effect
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.5),
+                  spreadRadius: 20,
+                  blurRadius: 50,
+                  offset: const Offset(0, -10),
+                ),
+              ],
+            ),
+            child: Image.asset("images/ball$ballNumber.png"),
+          ),
+          CustomPaint(
+            size: const Size(250, 250), // Kích thước tam giác
+            painter: TrianglePainter(),
+          ),
+          // Optionally, you can add a highlight overlay here
+          Positioned(
+            top: 50,
+            child: Container(
+              width: 120,
+              height: 120,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: RadialGradient(
+                  colors: [Colors.black.withOpacity(0.2), Colors.transparent],
+                  radius: 0.6,
+                  center: Alignment.topCenter,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class TrianglePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue.shade100.withOpacity(0.6)
+      ..style = PaintingStyle.fill;
+
+    // Dịch canvas lên trên 10 đơn vị
+    canvas.translate(0, -40);
+
+    // Xác định ba điểm của tam giác
+    final path = Path()
+      ..moveTo(size.width / 2, 0) // Đỉnh tam giác
+      ..lineTo(0, size.height) // Góc trái dưới
+      ..lineTo(size.width, size.height) // Góc phải dưới
+      ..close(); // Đóng đường path
+
+    // Vẽ tam giác lên canvas
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
